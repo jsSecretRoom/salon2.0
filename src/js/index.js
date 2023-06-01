60985b64dbff695140d275e3603616c1eae40d59
Rus1anGU <95142486+Rus1anGU@users.noreply.github.com>
2023-06-01 19:41:33 +0300

    slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,53 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
+  function sliderSwiper(buttonLeft, buttonRight, carousel, slides) {
+    const leftButtons = document.querySelectorAll(buttonLeft);
+    const rightButtons = document.querySelectorAll(buttonRight);
+    const sliderContainers = document.querySelectorAll(carousel);
 
-  function sliderSviper(buttonLeft, buttonRight, slaids){
-    const leftButton = document.querySelector(buttonLeft);
-    const rightButton = document.querySelector(buttonRight);
-    const slaidsFoto = document.querySelectorAll(slaids);
+    function slideToNext(container) {
+      const slideItems = container.querySelectorAll(slides);
+      const slideWidth = slideItems[0].offsetWidth;
 
+      container.style.transition = 'transform 0.5s ease';
+      container.style.transform = `translateX(-${slideWidth}px)`;
 
-    // Устанавливаем обработчики событий для кнопок
-    leftButton.addEventListener('click', slideToLeft);
-    rightButton.addEventListener('click', slideToRight);
-
-    // Функция для листания слайда влево
-    function slideToLeft() {
-      
+      setTimeout(function() {
+        container.appendChild(slideItems[0]);
+        container.style.transition = 'none';
+        container.style.transform = 'translateX(0)';
+      }, 500);
     }
-    // Функция для листания слайда вправо
-    function slideToRight() {
-      
+
+    function slideToPrev(container) {
+      const slideItems = container.querySelectorAll(slides);
+      const slideWidth = slideItems[0].offsetWidth;
+
+      const lastSlide = slideItems[slideItems.length - 1];
+      container.insertBefore(lastSlide, slideItems[0]);
+
+      container.style.transition = 'none';
+      container.style.transform = `translateX(-${slideWidth}px`;
+
+      setTimeout(function() {
+        container.style.transition = 'transform 0.5s ease';
+        container.style.transform = 'translateX(0)';
+      }, 50);
     }
 
+    leftButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        const container = button.closest('.wraper').querySelector('.carousel');
+        slideToPrev(container);
+      });
+    });
+
+    rightButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        const container = button.closest('.wraper').querySelector('.carousel');
+        slideToNext(container);
+      });
+    });
   }
-  sliderSviper('.left', '.right', '.foto-slid');
 
+  sliderSwiper('.left', '.right', '.carousel', '.foto-slid');
 });
